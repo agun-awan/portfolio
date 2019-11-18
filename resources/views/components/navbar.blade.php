2413c56879b3a08bdbf58879d6b971112fe549ac
Agun <axemindg@gmail.com>
2019-11-18 08:42:39 +0700

    Style update sidebar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="sidebar-header">
+<div id="sidebar-header">
     <h4>Agun Gunawan</h4>
     <div class="col-12">
         <img class="profile-image rounded-circle mb-3 mx-auto" src="https://keenthemes.com/preview/metronic/theme/assets/pages/media/profile/profile_user.jpg" alt="Best Pictures" />
@@ -6,28 +6,34 @@
     <p>Hi, my name is Agun Gunawan and I'm a senior Front-end Web Developer. Welcome to my personal website!</p>
     <div class="col-12">
         <ul class="social-list list-inline py-2 mx-auto">
-            <li class="list-inline-item"><a href="#"><i class="icofont-ui-email"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="icofont-facebook"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="icofont-github"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="icofont-linkedin"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="icon-style"><i class="icofont-ui-email"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="icon-style"><i class="icofont-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="icon-style"><i class="icofont-github"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="icon-style"><i class="icofont-linkedin"></i></a></li>
         </ul>
     </div>
 </div>
 
-<ul class="navbar-nav flex-column text-left list-menu">
-    <li class="active">
-        <a href="#"><i class="icofont-user"></i>&nbsp; About Me</a>
-    </li>
-    <li>
-        <a href="#"><i class="icofont-code"></i>&nbsp; Portfolio</a>
-    </li>
-    <li>
-        <a href="#">Abilities</a>
-    </li>
-    <li>
-        <a href="#">Portfolio</a>
-    </li>
-    <li>
-        <a href="#">Contact</a>
-    </li>
-</ul>
+<div id="sidebar-content">
+    <ul class="navbar-nav flex-column text-left list-menu">
+        <li class="active">
+            <a href="#"><i class="icofont-user"></i>&nbsp; About Me</a>
+        </li>
+        <li>
+            <a href="#"><i class="icofont-code"></i>&nbsp; Portfolio</a>
+        </li>
+        <li>
+            <a href="#"><i class="icofont-file-document"></i>&nbsp; Resume</a>
+        </li>
+        <li>
+            <a href="#"><i class="icofont-blogger"></i>&nbsp; Blogs</a>
+        </li>
+        <li>
+            <a href="#"><i class="icofont-contacts"></i>&nbsp; Contact</a>
+        </li>
+    </ul>
+</div>
+
+<div id="sidebar-footer">
+    <h5>This for footer content</h5>
+</div>
